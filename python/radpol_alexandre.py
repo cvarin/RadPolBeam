f8f0ebff13b3c250831b98d59c4659b3788eac6f
Charles Varin <cvarin@gmail.com>
2010-06-20 18:37:37 -0400

    Ajouté un article de doc qui va avec le fichier radpol_paraxial.py. Renormalisation de la solution d'Alexandre pour donner la bonne puissance.

--- python/radpol_alexandre.py
+++ python/radpol_alexandre.py
@@ -5,12 +5,16 @@
 from pylab import *
 from scipy.special import j0,jn
 
+eta0 = 120.0*pi
+mu0 = 12.566370614e-7
+eps0 = 8.854187817e-12
+
 ###########################################################
 # Champ d'après 
 # A.April, Opt. Lett. 331563 (2008)
 lambda0 = 0.8e-6
 k = 2.0*pi/lambda0
-a = 10.0/k
+a = 5.0/k
 wo = (sqrt(2.0)/k)*sqrt(sqrt(1.0+(k*a)**2)-1.0);
 
 print
@@ -22,13 +26,13 @@ print "*******************"
 print
 
 ###########################################################
-lim = 10.0
+lim = 40.0
 if (k*a <= 100): lim = 6.0
 if (k*a <= 70): lim = 4.0
 if (k*a <= 30): lim = 3.0
 if (k*a <= 10): lim = 2.0
 lim *= lambda0
-np = 1000
+np = 10000
 r = linspace(0,lim,np)
 dr = lim/np
 z = 0
@@ -41,29 +45,44 @@ kR = k*R
 # Champ électrique du TM01 [Eqs. (6) de A.April, Opt. Lett. 331563 (2008)]
 # E0 est défini à partir de la puissance 
 # [Eq. (12) de A. April, J. Opt. Soc. Am. A 27, 76-81 (2010)]
-eta0 = 120.0*pi
-P = 1.0e12
-E0 = sqrt(P*exp(2.0*k*a)*(4.0*eta0*k**2)/pi\
-     /(2.0*k*a*sinh(2.0*k*a) - cosh(2.0*k*a) + 1.0 - 2.0*(k*a)**2))
-print
-print "*******************"
-print "P = %.3e W" %P
-print "E0 = %.3e V/m" %E0
-print
-     
+E0 = 1.0e13
 H0 = E0/eta0
 H0p = 0.5*H0*k**3*wo**3
 E0p = eta0*H0p
 
-Hphi = H0p*sqrt(pi/kR)*jn(1+0.5,kR)*sin(theta)*exp(-k*a)
-Er = -0.5*1j*E0p*jn(2+0.5,kR)*sin(2.0*theta)*sqrt(pi/kR)*exp(-k*a)
+Hphi = H0p*sqrt(pi/kR/2.0)*jn(1+0.5,kR)*sin(theta)*exp(-k*a)
+Er = -0.5*1j*E0p*jn(2+0.5,kR)*sin(2.0*theta)*sqrt(pi/kR/2.0)*exp(-k*a)
 Ez = -2.0/3.0*1j*E0p*(jn(0+0.5,kR) + jn(2+0.5,kR)*0.25*(1.0+3.0*cos(2.0*theta)))\
-       *sqrt(pi/kR)*exp(-k*a)
+       *sqrt(pi/kR/2.0)*exp(-k*a)
+
+###########################################################
+# Puissance
+# Ref: A. April, J. Opt. Soc. Am. A 27, 76-81 (2010)
+###########################################################
+# Eq. (12)
+Pana = pi/(4.0*eta0*k**2)*E0**2*exp(-2.0*k*a)\
+    *(2.0*k*a*sinh(2.0*k*a) - cosh(2.0*k*a) + 1.0 - 2.0*(k*a)**2)
+    
+###########################################################
+# Eq. (13)
+Pparaxial = 2.0*(pi*a/(2.0*k))*E0**2/(4.0*eta0)
+
+###########################################################
+# Eq. (3)
+Pnum = 0
+for i in range(0,np):
+     Pnum += real(Er[i]*Hphi[i].conjugate())*r[i]
+Pnum*=0.5*2.0*pi*dr
+
+###########################################################
+# Normalisation du champ
+K00 = sqrt(Pana/Pnum)
+Hphi *= K00
+Er *= K00
+Ez *= K00
 
 ###########################################################
 # Densité d'énergie
-mu0 = 12.566370614e-7
-eps0 = 8.854187817e-12
 wr = 0.5*eps0*abs(Er)**2
 wz = 0.5*eps0*abs(Ez)**2
 wphi = 0.5*mu0*abs(Hphi)**2
@@ -74,11 +93,11 @@ figure(figsize=(16.0,8.5))
 tsize = 16
 maxi = max(wr + wz)
 rscl = sqrt(R**2 - (z + a*1j)**2)/lambda0
-title(r"$w_0 = %f \lambda_0,\quad ka = %f$" %(wo,k*a),size = tsize)
+title(r"$w_0 = %f \lambda_0,\quad ka = %f$" %(wo/lambda0,k*a),size = tsize)
 plot(rscl,wr/maxi,label=r"$|E_r|^2$")
 plot(rscl,wz/maxi,label=r"$|E_z|^2$")
 plot(rscl,(wr+wz)/maxi,label=r"$|E_r|^2 + |E_z|^2$")
-plot(rscl,wphi/maxi,label=r"$|H_\phi|^2$")
+#plot(rscl,wphi/maxi,label=r"$|H_\phi|^2$")
 xlabel(r"$r/\lambda$",size=tsize)
 ylabel(unicode("Champ au carré"),size=tsize)
 ylim(0,1)
@@ -88,27 +107,39 @@ legend(loc="best")
 
 ###########################################################
 # Puissance
-# Ref: A. April, J. Opt. Soc. Am. A 27, 76-81 (2010)
-# Eq. (12)
-P = pi/(4.0*eta0*k**2)*E0**2*exp(-2.0*k*a)\
-    *(2.0*k*a*sinh(2.0*k*a) - cosh(2.0*k*a) + 1.0 - 2.0*(k*a)**2)
-
-Pr = Pz = Pphi = 0
+Pnum = 0
 for i in range(0,np):
-     S = real(Er[i]*Hphi[i].conjugate())*r[i]
+     Pnum += real(Er[i]*Hphi[i].conjugate())*r[i]
+Pnum*=0.5*2.0*pi*dr
+print
+print "*******************"
+print "E0  = %.3e V/m" %E0
+print "K00 = %.3e" %(K00)
+print
+print "Pana  = %.3e W" %Pana
+print "Pnum  = %.3e W" %Pnum
+print "Ppara = %.3e W" %Pparaxial
+print
+print "Pana/Ppara = %.3e" %(Pana/Pparaxial)
+print "Pnum/Pana  = %.3e" %(Pnum/Pana)
+print
+
+###########################################################
+# Comparaison de l'énergie dans chaque composante.
+Pr = 0
+Pz = 0
+Pphi = 0
+for i in range(0,np):
      Pr += wr[i]*r[i]
      Pz += wz[i]*r[i]
      Pphi += wphi[i]*r[i]
-
-S*=0.5*dr*2.0*pi
-Pr*=dr*2.0*pi
-Pz*=dr*2.0*pi
-Pphi*=dr*2.0*pi
+Pr*=2.0*pi*dr
+Pz*=2.0*pi*dr
+Pphi*=2.0*pi*dr
 Ptot = Pr + Pz + Pphi
 ratio1 = real(Pr/Pz)
 ratio2 = real((Pr+Pz)/Pphi)
 
-print
 print "*******************"
 print "Pr   = %.3e " %real(Pr)
 print "Pz   = %.3e " %real(Pz)
@@ -116,16 +147,10 @@ print "Pphi = %.3e " %real(Pphi)
 print "Pr/Pz        = " + str(ratio1)
 print "(Pr+Pz)/Pphi = " + str(ratio2)
 print
-print "Ptheo   = %.3e W" %P
-print "S       = %.3e W" %S
-print "Ptheo/S = %.3e "  %(P/S)
-print "Ptot    = %.3e W" %(Pr + Pz + Pphi)
-print "S/Ptot  = %.3e " %(S/Ptot)
-print
-print "*******************"
-print
 
 ###########################################################
+print "*******************"
+print
 show()
 
 ######### Fin #############################################
---
+++ python/radpol_paraxial.py
@@ -0,0 +1,109 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+######### Modules #########################################
+from pylab import *
+from scipy.special import j0,jn
+
+eta0 = 120.0*pi
+c    = 2.99792458e8
+
+###########################################################
+# Champ d'après 
+lambda0 = 0.8e-6
+k = 2.0*pi/lambda0
+omega = k*c
+a = 300.0/k
+wo = (sqrt(2.0)/k)*sqrt(sqrt(1.0+(k*a)**2)-1.0);
+zR = 0.5*k*wo**2
+
+print
+print "*******************"
+print "lambda0    = " + str(lambda0)
+print "ka         = " + str(k*a)
+print "wo/lambda0 = " + str(wo/lambda0)
+print "zR/lambda0 = " + str(zR/lambda0)
+print "*******************"
+print
+
+###########################################################
+lim = 20.0
+if (k*a <= 400): lim = 14.0
+if (k*a <= 300): lim = 10.0
+if (k*a <= 100): lim = 6.0
+if (k*a <= 70): lim = 4.0
+if (k*a <= 30): lim = 3.0
+if (k*a <= 10): lim = 2.0
+lim *= lambda0
+np = 1000
+r = linspace(0,lim,np)
+dr = lim/np
+z = 0
+t = 0
+
+w = wo*sqrt(1+z**2/zR**2)
+R = z + zR**2/z
+phi_g = arctan(z/zR)
+phi_c = k*r**2/(2.0*R)
+psi = omega*t - k*z + 2.0*phi_g - phi_c
+
+###########################################################
+# Champ électrique du TM01 paraxial
+P = 1.0e12
+E0 = sqrt(4.0*eta0*P/pi*exp(-1.0)/wo**2)
+
+phi_0 = 0.0
+Er = E0*exp(0.5)*(sqrt(2.0)*r/wo)*(wo/w)**2*exp(-r**2/w**2)*cos(psi - phi_0)
+phi_0 = -pi/2.0
+Ez = E0*exp(0.5)*(2.0*sqrt(2.0)/(k*wo))*(wo/w)**2*exp(-r**2/w**2)\
+      *((1-r**2/w**2)*sin(psi - phi_0) - k*r**2/(2.0*R)*cos(psi - phi_0))
+
+print
+print "*******************"
+print "P = %.3e W" %P
+print "E0 = %.3e V/m" %E0
+print
+
+###########################################################
+# Densité d'énergie
+mu0 = 12.566370614e-7
+eps0 = 8.854187817e-12
+wr = 0.25*eps0*Er**2
+wz = 0.25*eps0*Ez**2
+
+###########################################################
+# Figure
+figure(figsize=(16.0,8.5))
+tsize = 16
+maxi = max(wr + wz)
+rscl = r/lambda0
+title(r"$w_0 = %f \lambda_0,\quad ka = %f$" %(wo/lambda0,k*a),size = tsize)
+plot(rscl,wr/maxi,label=r"$|E_r|^2$")
+plot(rscl,wz/maxi,label=r"$|E_z|^2$")
+plot(rscl,(wr+wz)/maxi,label=r"$|E_r|^2 + |E_z|^2$")
+xlabel(r"$r/\lambda$",size=tsize)
+ylabel(unicode("Champ au carré"),size=tsize)
+ylim(0,1)
+xlim(0,lim/lambda0)
+grid(True)
+legend(loc="best")
+
+###########################################################
+# Puissance
+Ptheo = E0**2/(2.0*eta0)*pi/2.0*wo**2*exp(1.0)
+
+P = 0.0
+for i in range(0,np):
+     P += Er[i]**2*r[i]
+P*=pi/eta0*dr
+
+print
+print "*******************"
+print "P   = %.3e W" %P
+print "Ptheo = %.3e W" %Ptheo
+print
+
+###########################################################
+show()
+
+######### Fin #############################################
