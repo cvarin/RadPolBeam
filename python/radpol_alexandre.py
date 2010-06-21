b98f0c3f349264b4e996a0d02e6a7321efad0065
Charles Varin <cvarin@gmail.com>
2010-06-21 11:04:52 -0400

    Harmonisé la solution d'Alexandre et la solution paraxiale (puissance et distribution du champ).

--- python/radpol_alexandre.py
+++ python/radpol_alexandre.py
@@ -8,13 +8,17 @@ from scipy.special import j0,jn
 eta0 = 120.0*pi
 mu0 = 12.566370614e-7
 eps0 = 8.854187817e-12
+eta0 = 120.0*pi
+c    = 2.99792458e8
+
+tsize = 16
 
 ###########################################################
 # Champ d'après 
 # A.April, Opt. Lett. 331563 (2008)
 lambda0 = 0.8e-6
 k = 2.0*pi/lambda0
-a = 5.0/k
+a = 350.0/k
 wo = (sqrt(2.0)/k)*sqrt(sqrt(1.0+(k*a)**2)-1.0);
 
 print
@@ -26,13 +30,15 @@ print "*******************"
 print
 
 ###########################################################
-lim = 40.0
+lim = 20.0
+if (k*a <= 400): lim = 14.0
+if (k*a <= 300): lim = 10.0
 if (k*a <= 100): lim = 6.0
 if (k*a <= 70): lim = 4.0
 if (k*a <= 30): lim = 3.0
 if (k*a <= 10): lim = 2.0
 lim *= lambda0
-np = 10000
+np = 1000
 r = linspace(0,lim,np)
 dr = lim/np
 z = 0
@@ -45,14 +51,22 @@ kR = k*R
 # Champ électrique du TM01 [Eqs. (6) de A.April, Opt. Lett. 331563 (2008)]
 # E0 est défini à partir de la puissance 
 # [Eq. (12) de A. April, J. Opt. Soc. Am. A 27, 76-81 (2010)]
-E0 = 1.0e13
-H0 = E0/eta0
-H0p = 0.5*H0*k**3*wo**3
-E0p = eta0*H0p
+#E0 = 1.0e13
+#H0 = E0/eta0
+#H0p = 0.5*H0*k**3*wo**3
+#E0p = eta0*H0p
 
-Hphi = H0p*sqrt(pi/kR/2.0)*jn(1+0.5,kR)*sin(theta)*exp(-k*a)
-Er = -0.5*1j*E0p*jn(2+0.5,kR)*sin(2.0*theta)*sqrt(pi/kR/2.0)*exp(-k*a)
-Ez = -2.0/3.0*1j*E0p*(jn(0+0.5,kR) + jn(2+0.5,kR)*0.25*(1.0+3.0*cos(2.0*theta)))\
+###########################################################
+# Puissance en régime paraxial : Eq. (13) de
+# A. April, J. Opt. Soc. Am. A 27, 76-81 (2010)
+P = 1.0e12
+E0 = sqrt(P/2.0/(pi*a/(2.0*k))*(4.0*eta0))
+E0p = E0
+H0p = E0p/eta0
+
+Hphi = -H0p*sqrt(pi/kR/2.0)*jn(1+0.5,kR)*sin(theta)*exp(-k*a)
+Er = 0.5*1j*E0p*jn(2+0.5,kR)*sin(2.0*theta)*sqrt(pi/kR/2.0)*exp(-k*a)
+Ez = 2.0/3.0*1j*E0p*(jn(0+0.5,kR) + jn(2+0.5,kR)*0.25*(1.0+3.0*cos(2.0*theta)))\
        *sqrt(pi/kR/2.0)*exp(-k*a)
 
 ###########################################################
@@ -82,15 +96,28 @@ Er *= K00
 Ez *= K00
 
 ###########################################################
+# Tracé des composantes
+fig1 = figure(figsize=(16.0,8.5))
+fig1.canvas.set_window_title(unicode("Distribution du champ électrique (non-paraxial)"))
+plot(r/lambda0,real(Er)/real(Er).max(),label="Er")
+plot(r/lambda0,real(-1j*Ez)/real(-1j*Ez).max(),label="Ez")
+xlabel(r"$r/\lambda$",size=tsize)
+ylabel(unicode("Champ électrique normalisé"),size=tsize)
+ylim(-0.2,1.0001)
+xlim(0,lim/lambda0)
+grid(True)
+legend(loc="best")
+
+###########################################################
 # Densité d'énergie
-wr = 0.5*eps0*abs(Er)**2
-wz = 0.5*eps0*abs(Ez)**2
-wphi = 0.5*mu0*abs(Hphi)**2
+wr = 0.25*eps0*abs(Er)**2
+wz = 0.25*eps0*abs(Ez)**2
+wphi = 0.25*mu0*abs(Hphi)**2
 
 ###########################################################
 # Figure
-figure(figsize=(16.0,8.5))
-tsize = 16
+fig2 = figure(figsize=(16.0,8.5))
+fig2.canvas.set_window_title(unicode("Densité d'énergie électrique (non-paraxial)"))
 maxi = max(wr + wz)
 rscl = sqrt(R**2 - (z + a*1j)**2)/lambda0
 title(r"$w_0 = %f \lambda_0,\quad ka = %f$" %(wo/lambda0,k*a),size = tsize)
@@ -133,9 +160,9 @@ for i in range(0,np):
      Pr += wr[i]*r[i]
      Pz += wz[i]*r[i]
      Pphi += wphi[i]*r[i]
-Pr*=2.0*pi*dr
-Pz*=2.0*pi*dr
-Pphi*=2.0*pi*dr
+Pr*=2.0*pi*dr*c
+Pz*=2.0*pi*dr*c
+Pphi*=2.0*pi*dr*c
 Ptot = Pr + Pz + Pphi
 ratio1 = real(Pr/Pz)
 ratio2 = real((Pr+Pz)/Pphi)
@@ -144,6 +171,10 @@ print "*******************"
 print "Pr   = %.3e " %real(Pr)
 print "Pz   = %.3e " %real(Pz)
 print "Pphi = %.3e " %real(Pphi)
+print "-------------------"
+print "Ptot = %.3e " %real(Ptot)
+print "-------------------"
+print
 print "Pr/Pz        = " + str(ratio1)
 print "(Pr+Pz)/Pphi = " + str(ratio2)
 print
--- python/radpol_paraxial.py
+++ python/radpol_paraxial.py
@@ -5,15 +5,19 @@
 from pylab import *
 from scipy.special import j0,jn
 
+mu0 = 12.566370614e-7
+eps0 = 8.854187817e-12
 eta0 = 120.0*pi
 c    = 2.99792458e8
+
+tsize = 16
 
 ###########################################################
 # Champ d'après 
 lambda0 = 0.8e-6
 k = 2.0*pi/lambda0
 omega = k*c
-a = 300.0/k
+a = 350.0/k
 wo = (sqrt(2.0)/k)*sqrt(sqrt(1.0+(k*a)**2)-1.0);
 zR = 0.5*k*wo**2
 
@@ -51,12 +55,26 @@ psi = omega*t - k*z + 2.0*phi_g - phi_c
 # Champ électrique du TM01 paraxial
 P = 1.0e12
 E0 = sqrt(4.0*eta0*P/pi*exp(-1.0)/wo**2)
-
 phi_0 = 0.0
-Er = E0*exp(0.5)*(sqrt(2.0)*r/wo)*(wo/w)**2*exp(-r**2/w**2)*cos(psi - phi_0)
-phi_0 = -pi/2.0
+Er = E0*exp(0.5)*(sqrt(2.0)*r/wo)*(wo/w)**2*exp(-r**2/w**2)*exp(1j*(psi - phi_0))
+#*cos(psi - phi_0)
+#phi_0 = -pi/2.0
 Ez = E0*exp(0.5)*(2.0*sqrt(2.0)/(k*wo))*(wo/w)**2*exp(-r**2/w**2)\
-      *((1-r**2/w**2)*sin(psi - phi_0) - k*r**2/(2.0*R)*cos(psi - phi_0))
+      *((1-r**2/w**2)*exp(1j*(psi - phi_0)) - k*r**2/(2.0*R)*exp(1j*(psi - phi_0)))
+      #*((1-r**2/w**2)*sin(psi - phi_0) - k*r**2/(2.0*R)*cos(psi - phi_0))
+
+###########################################################
+# Tracé des composantes
+fig1 = figure(figsize=(16.0,8.5))
+fig1.canvas.set_window_title(unicode("Distribution du champ électrique (paraxial)"))
+plot(r/lambda0,Er/max(Er),label="Er")
+plot(r/lambda0,Ez/max(Ez),label="Ez")
+xlabel(r"$r/\lambda$",size=tsize)
+ylabel(unicode("Champ électrique normalisé"),size=tsize)
+ylim(-0.2,1.001)
+xlim(0,lim/lambda0)
+grid(True)
+legend(loc="best")
 
 print
 print "*******************"
@@ -66,15 +84,15 @@ print
 
 ###########################################################
 # Densité d'énergie
-mu0 = 12.566370614e-7
-eps0 = 8.854187817e-12
-wr = 0.25*eps0*Er**2
-wz = 0.25*eps0*Ez**2
+wr = 0.25*eps0*Er*Er.conjugate()
+wz = 0.25*eps0*Ez*Ez.conjugate()
+#wr = 0.25*eps0*Er**2
+#wz = 0.25*eps0*Ez**2
 
 ###########################################################
 # Figure
-figure(figsize=(16.0,8.5))
-tsize = 16
+fig2 = figure(figsize=(16.0,8.5))
+fig2.canvas.set_window_title(unicode("Densité d'énergie électrique (paraxial)"))
 maxi = max(wr + wz)
 rscl = r/lambda0
 title(r"$w_0 = %f \lambda_0,\quad ka = %f$" %(wo/lambda0,k*a),size = tsize)
