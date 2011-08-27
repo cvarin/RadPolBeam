1af6f3c4a397c4f145e8c3c838bd1c006a6add07
cvarin <cvarin@gmail.com>
2011-08-27 00:27:18 -0400

    Ajouté un script pour évaluer la puissance seuil d'accélération.

---
+++ python/seuil.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from pylab import *
+from scipy import constants as cst
+
+lambda0 = 0.8e-6
+k = 2.0*pi/lambda0
+omega = k*cst.c
+#a = 278.581/k
+a = linspace(0,350)/k
+
+wo = (sqrt(2.0)/k)*sqrt(sqrt(1.0+(k*a)**2)-1.0);
+eta0 = 120.0*pi
+
+E0 = omega*cst.c*cst.m_e/cst.e
+
+Pana = pi/(4.0*eta0*k**2)*E0**2*exp(-2.0*k*a)\
+    *(2.0*k*a*sinh(2.0*k*a) - cosh(2.0*k*a) + 1.0 - 2.0*(k*a)**2)
+
+plot(wo/1.0e-6,Pana/1.0e12)
+
+
+a = linspace(350,2500)/k
+Pparaxial = 2.0*(pi*a/(2.0*k))*E0**2/(4.0*eta0)
+wo = (sqrt(2.0)/k)*sqrt(sqrt(1.0+(k*a)**2)-1.0);
+plot(wo/1.0e-6,Pparaxial/1.0e12)
+xscale("log")
+yscale("log")
+xlim(3,10)
+ylim(1,)
+show()
