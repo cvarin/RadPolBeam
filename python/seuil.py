9d9bb774f8cb021d3b00f8ec7268873f83055ab5
cvarin <cvarin@gmail.com>
2012-08-09 16:29:35 -0400

    Plus de points près de l'origine pour la solution paraxiale.

--- python/seuil.py
+++ python/seuil.py
@@ -18,15 +18,15 @@ E0 = omega*cst.c*cst.m_e/cst.e
 Pana = pi/(4.0*eta0*k**2)*E0**2*exp(-2.0*k*a)\
     *(2.0*k*a*sinh(2.0*k*a) - cosh(2.0*k*a) + 1.0 - 2.0*(k*a)**2)
 
-plot(wo/1.0e-6,Pana/1.0e12)
+plot(wo/1.0e-6,Pana/1.0e12,"r")
 
 
-a = linspace(350,2500)/k
+a = linspace(0,2500)/k
 Pparaxial = 2.0*(pi*a/(2.0*k))*E0**2/(4.0*eta0)
 wo = (sqrt(2.0)/k)*sqrt(sqrt(1.0+(k*a)**2)-1.0);
-plot(wo/1.0e-6,Pparaxial/1.0e12)
+plot(wo/1.0e-6,Pparaxial/1.0e12,"xb")
 xscale("log")
 yscale("log")
-xlim(3,10)
-ylim(1,)
+xlim(0,10)
+#ylim(1,)
 show()
